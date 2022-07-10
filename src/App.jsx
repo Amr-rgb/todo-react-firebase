a12071f48995a96dbbd609841b4db293520e2499
Amr Taha <amrshiha2@gmail.com>
2022-07-11 00:21:16 +0200

    basic app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,45 @@
+import { useState } from "react";
+import { Button, FormControl, Input, InputLabel } from "@material-ui/core";
+
 function App() {
+  const [todos, setTodos] = useState([
+    "Make a react firebase project",
+    "Record a coding video",
+  ]);
+
+  const [input, setInput] = useState("");
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    setTodos([...todos, input]);
+    setInput("");
+  };
+
   return (
     <div className="App">
       <h1>TODO React Firebase</h1>
+
+      <form>
+        <FormControl>
+          <InputLabel>Write a TODO</InputLabel>
+          <Input value={input} onChange={(e) => setInput(e.target.value)} />
+        </FormControl>
+        <Button
+          type="submit"
+          onClick={addTodo}
+          variant="contained"
+          color="primary"
+          disabled={!input}
+        >
+          Add Todo
+        </Button>
+      </form>
+
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }

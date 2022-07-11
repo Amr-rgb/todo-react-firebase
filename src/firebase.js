9f91e319dcb43ede4685762c0c68abcadb2ffd15
Amr Taha Abdulhaleem <61413839+Amr-rgb@users.noreply.github.com>
2022-07-11 20:28:04 +0200

    Delete firebase.js

--- src/firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBymwqSHQX4NKl3Gu7idWGRLmtD_f352Jk",
-  authDomain: "todo-react-firebase-40d4d.firebaseapp.com",
-  projectId: "todo-react-firebase-40d4d",
-  storageBucket: "todo-react-firebase-40d4d.appspot.com",
-  messagingSenderId: "771576978042",
-  appId: "1:771576978042:web:503bc48cb56b39b77079c3",
-};
